004ab5af0b9480bb11beb6adc1f364e9dd3f0c18
Madhav Sakhuja <msakhuja22@gmail.com>
2025-10-28 01:31:31 +0000

    Navbar_footer updated

--- day1/src/components/Footer/Footer.jsx
+++ day1/src/components/Footer/Footer.jsx
@@ -2,59 +2,49 @@ import './Footer.css'
 
 function Footer() {
     return <>
-        <footer className="site-footer pt-5 pb-1 border-0">
-            <div className="container">
-
-                <div className="row text-center text-md-start g-4 footer-columns">
-
-                    <div className="col-12 col-sm-6 col-md-3 d-flex flex-column footer-column">
-                        <h5>About</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="#" className="footer-link">About Us</a></li>
-                            <li><a href="#" className="footer-link">Careers</a></li>
-                            <li><a href="#" className="footer-link">Press</a></li>
+        <footer className="site-footer">
+            <div className="footer-content">
+                <div className="footer-columns">
+                    <div className="footer-column">
+                        <h4>About</h4>
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Careers</a></li>
+                            <li><a href="#">Press</a></li>
                         </ul>
                     </div>
-
-                    <div className="col-12 col-sm-6 col-md-3 d-flex flex-column footer-column">
-                        <h5>Discover</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="#" className="footer-link">Auctions</a></li>
-                            <li><a href="#" className="footer-link">Artists</a></li>
-                            <li><a href="#" className="footer-link">Galleries</a></li>
+                    <div className="footer-column">
+                        <h4>Discover</h4>
+                        <ul>
+                            <li><a href="#">Auctions</a></li>
+                            <li><a href="#">Artists</a></li>
+                            <li><a href="#">Galleries</a></li>
                         </ul>
                     </div>
-
-                    <div className="col-12 col-sm-6 col-md-3 d-flex flex-column footer-column">
-                        <h5>Resources</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="#" className="footer-link">Help Center</a></li>
-                            <li><a href="#" className="footer-link">Buying on Aurtistiq</a></li>
-                            <li><a href="#" className="footer-link">Selling on Aurtistiq</a></li>
+                    <div className="footer-column">
+                        <h4>Resources</h4>
+                        <ul>
+                            <li><a href="#">Help Center</a></li>
+                            <li><a href="#">Buying on Aurtistiq</a></li>
+                            <li><a href="#">Selling on Aurtistiq</a></li>
                         </ul>
                     </div>
-
-                    <div className="col-12 col-sm-6 col-md-3 d-flex flex-column footer-column">
-                        <h5>Stay in Touch</h5>
-                        <form className="footer-newsletter d-flex">
-                            <input type="email" className="form-control" placeholder="Your's Email" required />
-                            <button className="subscribe-btn" type="submit">Subscribe </button>
+                    <div className="footer-column footer-newsletter">
+                        <h4>Stay in Touch</h4>
+                        <form id="footer-newsletter" className="footerForm">
+                            <input id="email" type="email" placeholder="Your email address" required />
+                            <button type="submit">Subscribe</button>
                         </form>
                     </div>
                 </div>
-
-                <hr className="mt-3 mb-2" />
-
-                <div className="d-flex flex-column flex-md-row justify-content-between align-items-center footer-bottom">
-                    <p className="mb-2 mb-md-0">© 2025 Aurtistiq. All rights reserved.</p>
-
+                <div className="footer-bottom">
+                    <p>© 2025 Aurtistiq. All rights reserved.</p>
                     <div className="footer-social">
                         <a href="#"><i className="fab fa-facebook-f"></i></a>
                         <a href="#"><i className="fab fa-instagram"></i></a>
                         <a href="#"><i className="fab fa-twitter"></i></a>
                     </div>
                 </div>
-
             </div>
         </footer>
     </>
--- day1/src/components/Navbar/Navbar.jsx
+++ day1/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,27 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
+    <nav className="navbar">
       <div className="container-fluid d-flex align-items-center justify-content-between">
+        
+        {/* Left: Logo + Brand */}
         <div className="d-flex align-items-center">
           <img src="/logo.png" alt="logo" className="navbar-logo" />
           <NavLink className="navbar-brand" to="/">
             Aurtistiq
           </NavLink>
         </div>
+
+        {/* Center: Search bar */}
         <form className="search-bar" role="search">
           <input
             className="search-input"
@@ -19,26 +30,31 @@ function Navbar() {
             aria-label="Search"
           />
         </form>
+
+        {/* Right: Hamburger (mobile) */}
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
+          className={`custom-toggler ${isMenuOpen ? "active" : ""}`}
+          onClick={toggleMenu}
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="custom-hamburger"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <div className="navbar-links d-flex align-items-center">
-            <NavLink className="nav-link" to="/">Home</NavLink>
-            <NavLink className="nav-link" to="/Collection">Collect</NavLink>
-            <NavLink className="nav-link" to="/Auction">Auctions</NavLink>
-            <NavLink className="nav-link" to="/Sign-In">Sign In</NavLink>
-          </div>
-        </div>
 
+        {/* Desktop links */}
+        <div className="navbar-links d-none d-lg-flex align-items-center">
+          <NavLink className="nav-link" to="/">Home</NavLink>
+          <NavLink className="nav-link" to="/Collection">Collect</NavLink>
+          <NavLink className="nav-link" to="/Auction">Auctions</NavLink>
+          <NavLink className="nav-link" to="/Sign-In">Sign In</NavLink>
+        </div>
+      </div>
+
+      {/* Mobile dropdown menu (below navbar) */}
+      <div className={`dropdown-menu-panel ${isMenuOpen ? "open" : ""}`}>
+        <NavLink onClick={toggleMenu} className="dropdown-link" to="/">Home</NavLink>
+        <NavLink onClick={toggleMenu} className="dropdown-link" to="/Collection">Collect</NavLink>
+        <NavLink onClick={toggleMenu} className="dropdown-link" to="/Auction">Auctions</NavLink>
+        <NavLink onClick={toggleMenu} className="dropdown-link" to="/Sign-In">Sign In</NavLink>
       </div>
     </nav>
   );
